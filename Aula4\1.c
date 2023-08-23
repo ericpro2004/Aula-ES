e3cf0d938fee1e9275a5622512e38c5921a6818d
ericpro2004 <126572065+ericpro2004@users.noreply.github.com>
2023-08-23 10:29:36 -0300

    Update Aula4\1.c

--- Aula4\1.c
+++ Aula4\1.c
@@ -5,6 +5,7 @@
 struct no {
     int numero;
     struct no *proximo;
+    struct no *anterior;
 };
 
 struct no *cabeca;
@@ -47,15 +48,46 @@ void imprimir(){
 void excluir(int numero) {
     setlocale(LC_ALL, "");
 
+        if (cabeca != NULL) {
+            if (cabeca -> numero == numero) {
+                if (cabeca -> proximo == cabeca) {
+                    free(cabeca);
+                    cabeca = NULL;
+                } else if {
+                        struct no *ponteiroExcluir = cabeca;
+                        cabeca = cabeca -> proximo;
+
+                        struct no *ponteiroAnterior = cabeca;
+
+                        while (ponteiroAnterior -> proximo != ponteiroExcluir) {
+                        ponteiroAnterior = ponteiroAnterior -> proximo;
+
+                        ponteiroAnterior -> proximo = cabeca;
+                        free (ponteiroExcluir);
+                    }
+                } else {
+                    struct no *ponteiroAnterior = cabeca;
+
+                    while ((ponteiroAnterior -> proximo != cabeca) && (ponteiroAnterior -> proximo -> numero != numero)) {
+                        ponteiroAnterior = ponteiroAnterior -> proximo;
+                    }
+
+                if (ponteiroAnterior -> proximo != cabeca) {
+                    struct no *ponteiroExcluir = ponteiroAnterior -> proximo;
+                    ponteiroAnterior -> proximo = ponteiroExcluir -> proximo;
+                    free(ponteiroExcluir);
+            }
+        }
+    }
 }
 
 int main(){
 
-    //excluir(1);
+    excluir(1);
     imprimir();
 
     inserir(1);
-    //excluir(1);
+    excluir(1);
     imprimir();
 
     inserir(1);
@@ -63,9 +95,9 @@ int main(){
     inserir(3);
     inserir(4);
     inserir(5);
-    //excluir(1);
-    //excluir(5);
-    //excluir(3);
+    excluir(1);
+    excluir(5);
+    excluir(3);
     imprimir();
 
 
